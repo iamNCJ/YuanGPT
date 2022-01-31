e877e712a1c649ee0d0c552232dea603adee896d
NCJ <me@ncj.wiki>
2022-01-31 12:52:38 +0800

    working tokenizer

--- data/preprocess/preprocess_cn.py
+++ data/preprocess/preprocess_cn.py
@@ -2,6 +2,11 @@
 
 import collections
 
+import jieba
+import numpy as np
+from tqdm import tqdm
+from tokenizers.implementations import BertWordPieceTokenizer
+
 
 def convert_to_unicode(text):
     """Converts `text` to Unicode (if it's not already), assuming utf-8 input."""
@@ -28,6 +33,32 @@ def load_vocab(vocab_file):
     return vocab
 
 
+def do_tokenize():
+    with open('../example_data/001.txt', 'r', encoding='utf-8') as f:
+        lines = f.readlines()
+    tokenizer = BertWordPieceTokenizer(vocab='./vocab.txt')
+    for line in tqdm(lines):
+        line = line.strip()
+        if line != '':
+            _res = np.zeros(shape=(3072, ), dtype=np.int32)
+            i = 0
+            for x in jieba.cut(line, cut_all=False):
+                # _x = tokenizer.token_to_id(x)
+                # if _x is not None:
+                #     _res[i] = _x
+                #     i += 1
+                # if i == 3072:
+                #     break
+                try:
+                    _res[i] = res[x]
+                    i += 1
+                    if i == 3072:
+                        break
+                except KeyError:
+                    pass
+
+
 if __name__ == '__main__':
     res = load_vocab('./vocab.txt')
     print(len(res))
+    do_tokenize()
