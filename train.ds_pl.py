b6e3b0951996623038b95470cf4dc3c579ae1b9c
NCJ <me@ncj.wiki>
2022-02-05 16:38:40 +0800

    Update train.ds_pl.py

--- train.ds_pl.py
+++ train.ds_pl.py
@@ -1,5 +1,4 @@
 import torch
-from pytorch_lightning.plugins import DeepSpeedPlugin
 
 from config import LMConfig
 from data import MockDataModule
@@ -7,48 +6,11 @@ from model import NativeModel
 from trainer.lightning import pl_train
 
 if __name__ == '__main__':
-    deepspeed_config = {
-        # "zero_allow_untested_optimizer": True,
-        "train_batch_size": 2,
-        "bf16": {
-            "enabled": True
-        },
-        "optimizer": {
-            "type": "Adam",
-            "params": {
-                "lr": 0.001,
-                "betas": [
-                    0.8,
-                    0.999
-                ],
-                "eps": 1e-8,
-                "weight_decay": 3e-7
-            }
-        },
-        "scheduler": {
-            "type": "WarmupLR",
-            "params": {
-                "last_batch_iteration": -1,
-                "warmup_min_lr": 0,
-                "warmup_max_lr": 3e-5,
-                "warmup_num_steps": 100,
-            },
-        },
-        "zero_optimization": {
-            "stage": 3,
-            # "offload_optimizer": {
-            #     "device": "cpu",
-            #     "pin_memory": True,
-            #     "buffer_count": 4,
-            #     # "fast_init": False
-            #   },
-        },
-    }
 
     mock_config = LMConfig(
         vocab_size=53228,
         hidden_size=3072,
-        layer_num=15,
+        layer_num=14,
         attention_heads=16,
         seq_length=2048,
         learning_rate=0.0001,
@@ -68,5 +30,4 @@ if __name__ == '__main__':
         max_epochs=1,
         strategy='deepspeed_stage_3',
         num_nodes=4,
-        # DeepSpeedPlugin(config=deepspeed_config, logging_batch_size_per_gpu=mock_config.batch_size),
     )
