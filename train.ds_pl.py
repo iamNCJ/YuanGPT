d7e9fe992081f7ecd42fe936b5016279c6da714c
NCJ <me@ncj.wiki>
2022-02-05 17:41:37 +0800

    change to huggingface model

--- train.ds_pl.py
+++ train.ds_pl.py
@@ -2,7 +2,7 @@ import torch
 
 from config import LMConfig
 from data import MockDataModule
-from model import NativeModel
+from model import HFModel
 from trainer.lightning import pl_train
 
 if __name__ == '__main__':
@@ -16,7 +16,7 @@ if __name__ == '__main__':
         learning_rate=0.0001,
         batch_size=1,
     )
-    core_model = NativeModel(mock_config)
+    core_model = HFModel(mock_config)
     dm = MockDataModule(
         vocab_size=mock_config.vocab_size,
         seq_length=mock_config.seq_length,
