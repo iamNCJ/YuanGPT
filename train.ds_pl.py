fa6525705bb6244ae07dae4995bc7a1b26b9eb93
NCJ <me@ncj.wiki>
2022-02-08 23:50:13 +0800

    Update train.ds_pl.py

--- train.ds_pl.py
+++ train.ds_pl.py
@@ -14,7 +14,7 @@ if __name__ == '__main__':
         attention_heads=24,
         seq_length=2048,
         learning_rate=0.0001,
-        batch_size=16,
+        batch_size=20,
     )
     core_model = HFModel(mock_config)
     dm = MockDataModule(
