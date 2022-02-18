1b9f2fda255bb41179427c395ae0405aece94833
NCJ <me@ncj.wiki>
2022-02-18 11:14:35 +0800

    Update model_wrapper.py

--- trainer/lightning/model_wrapper.py
+++ trainer/lightning/model_wrapper.py
@@ -53,11 +53,11 @@ class LitModel(pl.LightningModule):
     def configure_optimizers(self):
         # Do Adam on CPU when offloading
         if self.strategy.use_offload:
-            from torch.optim import Adam
+            # from torch.optim import Adam
             # from torch.optim._multi_tensor import Adam
-            return Adam(self.model.parameters(), lr=self.model.config.learning_rate)
-            # from deepspeed.ops.adam import DeepSpeedCPUAdam
-            # return DeepSpeedCPUAdam(self.parameters(), lr=self.model.config.learning_rate)
+            # return Adam(self.model.parameters(), lr=self.model.config.learning_rate)
+            from deepspeed.ops.adam import DeepSpeedCPUAdam
+            return DeepSpeedCPUAdam(self.parameters(), lr=self.model.config.learning_rate)
         # Use FusedAdam when ZeRO is on and offload is not used, which reduces optimizer state
         elif self.strategy.use_deepspeed_zero:
             from deepspeed.ops.adam import FusedAdam
