e22111dc314a9336da2525e8806feb82742b91e6
NCJ <me@ncj.wiki>
2022-02-19 23:21:10 +0800

    Update trainer_wrapper.py

--- trainer/lightning/trainer_wrapper.py
+++ trainer/lightning/trainer_wrapper.py
@@ -37,7 +37,6 @@ def train(
         log_every_n_steps=1,
         enable_model_summary=False,
         strategy=use_distributed.pl_strategy,
-        accumulate_grad_batches=1,
         **kwargs
     )
     trainer.fit(wrapper_model, data_module)
