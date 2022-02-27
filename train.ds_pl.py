35dab7a890625ca36ecc814bf3ed9a06f509066a
NCJ <me@ncj.wiki>
2022-02-27 22:03:28 +0800

    test no activation

--- train.ds_pl.py
+++ train.ds_pl.py
@@ -14,7 +14,7 @@ if __name__ == '__main__':
         attention_heads=24,
         seq_length=2048,
         learning_rate=5e-5,
-        batch_size=16,
+        batch_size=1,
     )
     core_model = HFModel(config)
     dm = YuanDataModule(
@@ -23,7 +23,7 @@ if __name__ == '__main__':
     )
     pl_train(
         core_model, dm,
-        use_distributed=DistributedStrategy.CUSTOM,
+        use_distributed=DistributedStrategy.DEEPSPEED_STAGE_3_OFFLOAD,
         gpus=-1 if torch.cuda.is_available() else None,
         precision=16,
         max_epochs=1,
