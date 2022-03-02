dc396d494c9f41cb99854c30cf4d8c6dbcbd5f9e
NCJ <me@ncj.wiki>
2022-03-03 00:04:19 +0800

    Update data_module.py

--- data/yuan/data_module.py
+++ data/yuan/data_module.py
@@ -35,7 +35,7 @@ class YuanDataModule(pl.LightningDataModule):
         if self.dataset is None:
             npz_data = np.load(self.processed_data_path)
             ids = torch.from_numpy((npz_data['id'])[0:(488282 + 100)].astype(np.int64))
-            print(f'max id = {torch.max(ids)}')
+            # print(f'max id = {torch.max(ids)}')
             # attention_masks = torch.from_numpy(npz_data['attention_mask'].astype(np.int64))
             dataset = TensorDataset(ids)
             train_dataset, val_dataset = random_split(dataset, [488282, 100])
